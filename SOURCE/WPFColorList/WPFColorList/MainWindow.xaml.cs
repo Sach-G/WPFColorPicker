d398e70c0ccaea9deacb7d948cab19b3d517e6b9
Sach-G <sachintha.gurudeniya@screen-spe.com>
2022-11-14 15:20:07 -0800

    Initial code

---
+++ SOURCE/WPFColorList/WPFColorList/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows;
+
+namespace WPFColorList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<SystemColor> Colors { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            InitColors();
+        }
+
+        public void InitColors()
+        {
+            Colors = new List<SystemColor>();
+            var colors = Enum.GetValues(typeof(KnownColor));
+            foreach (KnownColor knowColor in colors)
+            {
+                Color color = Color.FromKnownColor(knowColor);
+                if (!color.IsSystemColor)
+                    Colors.Add(new SystemColor(color));
+            }
+            Colors.Sort((x, y) => x.HexA.CompareTo(y.HexA));
+        }
+
+        private void MenuItem_Click_Name(object sender, RoutedEventArgs e)
+        {
+            var selected = (SystemColor)DgvColors.SelectedValue;
+            Clipboard.SetText(selected.ColorName);
+        }
+
+        private void MenuItem_Click_Hex(object sender, RoutedEventArgs e)
+        {
+            var selected = (SystemColor)DgvColors.SelectedValue;
+            Clipboard.SetText(selected.Hex);
+        }
+
+        private void MenuItem_Click_RGB(object sender, RoutedEventArgs e)
+        {
+            var selected = (SystemColor)DgvColors.SelectedValue;
+            Clipboard.SetText(selected.RGB);
+        }
+
+        private void MenuItem_Click_Details(object sender, RoutedEventArgs e)
+        {
+            var selected = (SystemColor)DgvColors.SelectedValue;
+            Clipboard.SetText(string.Format("{0} - ({1}) ({2})", selected.ColorName, selected.Hex, selected.RGB));
+        }
+    }
+}
---
+++ SOURCE/WPFColorList/WPFColorList/SystemColor.cs
@@ -0,0 +1,46 @@
+﻿using System.Drawing;
+
+namespace WPFColorList
+{
+    public class SystemColor
+    {
+        public Color Color { get; set; }
+        public string ColorName
+        {
+            get
+            {
+                return Color.Name;
+            }
+        }
+
+        public string Hex { get { return Color.ToHexString(); } }
+
+        public string HexA { get { return Color.ToHexAString(); } }
+
+        public string RGB { get { return Color.ToRGBString(); } }
+
+        public string RGBA { get { return Color.ToRGBAString(); } }
+
+        public SystemColor(Color color)
+        {
+            Color = color;
+        }
+    }
+
+    public static class ColorConverterExtensions
+    {
+        // #RRGGBB
+        public static string ToHexString(this Color c) => $"#{c.R:X2}{c.G:X2}{c.B:X2}";
+
+        // #RRGGBBAA
+        public static string ToHexAString(this Color c) => $"#{c.R:X2}{c.G:X2}{c.B:X2}{c.A:X2}";
+
+        // RGB(R, G, B)
+        public static string ToRGBString(this Color c) => $"{c.R}, {c.G}, {c.B}";
+
+        // RGBA(R, G, B, A)
+        public static string ToRGBAString(this Color c) => $"{c.R}, {c.G}, {c.B}, {ToProportion(c.A):N2}";
+
+        public static double ToProportion(byte b) => b / (double)byte.MaxValue;
+    }
+}
